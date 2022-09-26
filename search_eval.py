59a55453e54f313489c050308dea10f3e46a0543
Ethan Alberto <35020565+ethannakamura1211@users.noreply.github.com>
2022-09-26 23:14:16 +0100

    Update search_eval.py

--- search_eval.py
+++ search_eval.py
@@ -4,6 +4,45 @@ import time
 import metapy
 import pytoml
 
+class mptf2lnRanker(metapy.index.RankingFunction):
+    """
+    Create a new ranking function in Python that can be used in MeTA.
+    """
+    def __init__(self, some_param=1.0):
+        self.param = some_param
+        # You *must* call the base class constructor here!
+        super(mptf2lnRanker, self).__init__()
+
+    def score_one(self, sd):
+        """
+        You need to override this function to return a score for a single term.
+        For fields available in the score_data sd object,
+        @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
+        """
+        doc_len = sd.idx.doc_size(sd.d_id)
+        avg_dl = sd.avg_dl
+        tf = sd.doc_term_count 
+        df = sd.doc_count 
+        pc = float(sd.corpus_term_count) / sd.total_terms #this is p(t/C)
+
+        s = 0.4 
+        mu = 2000 
+        alpha = 0.2 
+        lambdap = 0.6
+
+        tfok = 2.2*tf/(1.2+tf)
+        idfpiv = math.log((sd.num_docs + 1.0)/df)
+        tfidfdir = math.log(1.0 + tf/(mu*pc))
+        lnpiv = 1 - s + s*doc_len/avg_dl
+
+        tfidf2 = alpha*tfok*idfpiv + (1.0 - alpha)*tfidfdir
+
+        score = sd.query_term_weight*tfidf2/math.pow(lnpiv,lambdap)
+
+        return score
+       # tfn = sd.doc_term_count * math.log((1+(sd.avg_dl/sd.doc_size)),2)\
+       # return (self.param + sd.doc_term_count) / (self.param * sd.doc_unique_terms + sd.doc_size)
+       # return sd.query_term_weight * (tfn / (tfn + self.param)) * math.log(((sd.num_docs + 1) / (sd.corpus_term_count + 0.5)),2)
 
 def load_ranker(cfg_file):
     """
@@ -11,7 +50,8 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25(k1=1.25,b=0.55,k3=400)
+    # return metapy.index.OkapiBM25(k1=1.25,b=0.55,k3=400)
+    return mptf2lnRanker()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
